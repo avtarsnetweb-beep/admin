cd3d92bf1c317dac049c5224cb5cb94b7a699f8d
Avtar Singh <avtar.s.netweb@gmail.com>
2025-11-15 11:49:29 +0530

    api

--- new-backend/index.js
+++ new-backend/index.js
@@ -13,7 +13,7 @@ const corsOptions = {
     "https://auth-project-avtar.netlify.app"
   ],
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"]
 };
 
