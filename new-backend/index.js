b8b10ab51a3a58d5595eeb4e4a44aeddedf0b033
Avtar Singh <avtar.s.netweb@gmail.com>
2025-11-15 14:31:04 +0530

    changes

--- new-backend/index.js
+++ new-backend/index.js
@@ -10,7 +10,8 @@ const app = express();
 const corsOptions = {
   origin: [
     "http://localhost:5173",
-    "https://auth-project-avtar.netlify.app"
+    "https://auth-project-avtar.netlify.app",
+   " https://stalwart-praline-bbbe6d.netlify.app"
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
